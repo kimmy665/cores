cba643e238d2ad32c5fa4854d6e34689ac5a9aba
Farzad Asgari <khufarzadasgari@gmail.com>
2025-09-03 17:26:40 +0330

    add tailwind configs to project

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,9 @@
 const Index = () => {
-    return <h1>CoreS | Admin Panel</h1>;
+    return (
+        <h1 className="text-3xl font-bold underline text-rose-600">
+            CoreS | Admin Panel
+        </h1>
+    );
 };
 
 export default Index;
